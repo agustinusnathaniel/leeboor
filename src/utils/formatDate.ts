0d1313a6ab4179123153788c2ffcba0b8b2349a8
Agustinus Nathaniel <berserk.nate@gmail.com>
2025-12-25 22:28:02 +0700

    chore: update packages to latest

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,7 @@
 import { format } from 'date-fns';
 // date-fns #2629 & #2694
 // https://github.com/date-fns/date-fns/issues/2964#issuecomment-1397856471
-import id from 'date-fns/locale/id/index';
+import { id } from 'date-fns/locale';
 
 export const formatDate = (date: Date | string, formatting?: string) =>
   format(new Date(date), formatting ?? 'eeee, dd MMMM yyyy', {
